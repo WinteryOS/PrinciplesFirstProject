ca604a32a7c466465c77d005d71548dbd82ee4bd
nicolesad <nsad308@gmail.com>
2021-07-15 19:43:11 -0600

    Stone styling and functionality progress

--- levelsdraft1.js
+++ levelsdraft1.js
@@ -11,13 +11,17 @@ function createRows() {
 var max = table.rows.length + 2;
 for(var i = 0; i < 6; i++) {
     var row = document.createElement('tr');
+    row.setAttribute('class', `row${i}`);
     table.appendChild(row);
     for(var j = 0; j < max-1; j++) {
         var cell = document.createElement('td');
         cell = row.insertCell(j);
         var button = document.createElement('input');
+        button.addEventListener("click", disableRows(event));
+        button.setAttribute('class', 'stonebtn');
+        button.setAttribute('class', `stonerow${i}`);
         button.setAttribute('type', 'button');
-        button.setAttribute('value', 'test');
+        button.setAttribute('id', `btn${i}${j}`);
         cell.appendChild(button);
     }
     max += 2;
@@ -25,19 +29,35 @@ for(var i = 0; i < 6; i++) {
 tablediv.appendChild(table);
 }
 
-//click button to select/unselect
-//when one button in row is selected, disable others not in row
+function disableRows(event) {
+    // //disable stones in all other rows (check stone's tr id? or make stone part of its own class)
+    // var stone = event.target;
+    // //stone.tagname...
+    // for(i = 0, row; row=table.row[i]; i++) {
+    //     if (stone
+    // }
+    //     //foreach row.. if (row.id != stone's row.id), foreach stone in row, disable click & make less visible
 
-//if easy, show rows 1 3 5
+}
 
-//if medium, show easy rows and row 7
+function setDifficulty(difficulty) {
+    //if difficulty == easy
+        //show rows 1 3 5
+    //elif diff == medium
+        //show rows easy, show row 7
+    //elif diff == hard
+        //show rows medium, hide row 1, show rows 9 11
+}
 
-//if hard, show medium rows, hide row 1, show rows 9 11
+//GOALS FOR THURSDAY:
+    //center gameplay, put in game area and commit to master
+    //add button for switching players/confirming selection
+    // ^^ for de/selecting all stones in a row
+    //get functionality for disability of buttons when pressed
 
 
 
 
-//rows are made of buttons
 //when first stone pressed, highlight stone (and row?)
 //allow clicks until last stone in row is gone
 //when "finalize" button is pressed..
