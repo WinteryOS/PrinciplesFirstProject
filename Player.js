c784db6bbd8a1ef3e7b2f725ccfc46ab1f439733
mirandashaekelley <mkelley@student.neumont.edu>
2021-07-18 15:56:34 -0600

    Game Mode Implemented + Kia's Work

--- Player.js
+++ Player.js
@@ -6,28 +6,34 @@ var hasPlayerMoved = false;
 var gameOver = false;
 //Form Variable
 const form = document.getElementById('form');
+//Game Difficulty Variable
+var gameDifficulty;
+
+var turnBoxText = document.getElementById("turnBox");
 
 //Event Listeners
 form.addEventListener('submit', submitNames);
 
 
 //Assign Player Names
-function submitNames(){
+function submitNames(p1Name, p2Name){
     event.preventDefault();
     
     var p1Name = document.getElementById('p1Name').value;
     if(p1Name === ""){
-        var p1Name = "Player 1"
+        p1Name = "Player 1"
     }
     console.log(p1Name);
 
     var p2Name = document.getElementById('p2Name').value;
     if(p2Name === ""){
-        var p2Name = "Player 2"
+        p2Name = "Player 2"
     }
     console.log(p2Name);
-//Start Game Function Would go here
-    document.getElementById("turnBox").innerHTML = `${p1Name}'s Turn`;
+
+    StartGame(p1Name,p2Name);
+
+   
 }
 
 //End Turn Button Function
@@ -37,17 +43,58 @@ function submitNames(){
 //Who's Turn it is box
 
 
-var StartGame = function(){
-    player = 1;
-    gameOver = false;
-    hasPlayerMoved = false;
+function StartGame(p1Name,p2Name){
+    turnBoxText.innerText = p1Name + "'s Turn";
+
+    switch(gameDifficulty) {
+        case 'Easy':
+            EasyMode();
+          break;
+        case 'Medium':
+            MediumMode();
+          break;
+        case 'Hard':
+            HardMode();
+            break;
+        default:
+          console.log("Default Error");
+      }
 
 }
+//Easy Mode
+function EasyMode(){
+    console.log(gameDifficulty);
+}
+//Medium Mode
+function MediumMode(){
+    console.log(gameDifficulty);
+}
+//Hard Mode
+function HardMode(){
+    console.log(gameDifficulty);
+}
 
-var switchPlayer = function(){
+function SwitchPlayer(p1Name,p2Name){
     if(!gameOver) {
         if(player === 1){
             player = 2;
-        } 
+            turnBoxText.innerText = p2Name + "'s Turn";
+            
+        } else {
+            player = 1;
+            turnBoxText.innerText = p1Name + "'s Turn";
+        }
     }
+    console.log(player);
 }
+//Functions by Kia
+function openForm() {
+    form.style.display = "block";
+  }
+  
+  function closeForm() {
+    form.style.display = "none";
+    //Miranda
+    gameDifficulty = document.getElementById("gameDifficulty").value;
+    
+  }
